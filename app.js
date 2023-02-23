9534109159c3a3a0f424be0903a204eeb657bc56
Chris V <119519387+c1846946@users.noreply.github.com>
2023-02-23 10:11:04 -0500

    Connect cellButtons to the board array

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function Gameboard() {
 }
 
 function Cell() {
-  let value = 0;
+  let value = " ";
 
   const markCell = (player) => {
     value = player;
@@ -75,7 +75,7 @@ function gameController() {
   } */
 
   const takeTurn = (column, row) => {
-    if (board.getBoard()[column][row].getValue() !== 0) return;
+    if (board.getBoard()[column][row].getValue() !== " ") return;
     console.log(`${getActivePlayer().name} is marking ${column}, ${row} `);
     board.playerMark(column, row, getActivePlayer().name);
 
@@ -100,18 +100,33 @@ function ScreenController() {
   //display who's turn
 
   //render board squares
-  board.forEach((row) => {
-    row.forEach((cell, index) => {
-      const cellButton = document.createElement("button");
-      cellButton.classList.add("cell");
-      cellButton.dataset.xcoordinate = index;
-      cellButton.dataset.ycoordinate = board.indexOf(row);
-      cellButton.textContent = cell.getValue();
-      boardDiv.appendChild(cellButton);
+  const updateScreen = () => {
+    boardDiv.textContent = "";
+
+    board.forEach((row) => {
+      row.forEach((cell, index) => {
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        cellButton.dataset.rowIndex = index;
+        cellButton.dataset.colIndex = board.indexOf(row);
+        cellButton.textContent = cell.getValue();
+        boardDiv.appendChild(cellButton);
+      });
     });
-  });
+  };
   //for each cell, create a button, with coordinate dataset
   //use css grid to organize the cells
+
+  function boardClickHandler(e) {
+    console.log(e.target.dataset.colIndex);
+    col = e.target.dataset.colIndex;
+    row = e.target.dataset.rowIndex;
+
+    game.takeTurn(col, row);
+    updateScreen();
+  }
+  updateScreen();
+  boardDiv.addEventListener("click", boardClickHandler);
 }
 
 ScreenController();
